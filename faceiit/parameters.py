a0326c2d79ffb7b7dcab5c2185b70ca7f1bcb572
Nikunj Mistry <nikunjmistry1000@gmail.com>
2022-09-14 17:44:26 +0530

    Webscraping in Linkedin

---
+++ faceiit/parameters.py
@@ -0,0 +1,10 @@
+# search query
+search_query = 'https://www.linkedin.com/search/results/people/?geoUrn=%5B%22101165590%22%5D&heroEntityKey=urn%3Ali%3Aautocomplete%3A2088825760&keywords=working%20professional&origin=FACETED_SEARCH&position=0&searchId=acf7b068-559e-4848-95ff-22ac6dc7d9ff&sid=oq%3B'
+    #'site:linkedin.com/in/ "people"'
+
+# file were scraped profile information will be stored
+file_name = 'results_file1.csv'
+
+# login credentials
+linkedin_username = 'nikunjmistry1000@gmail.com'
+linkedin_password = 'nikunj22'
